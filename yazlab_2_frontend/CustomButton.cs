abd0641bf221dd0c5f152893f4dfc9cfdbdeb1d5
Alper <Alperhakli2004@gmail.com>
2025-12-17 19:01:00 +0300

    Initial Commit

---
+++ yazlab_2_frontend/CustomButton.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yazlab_2_frontend
+{
+    public class CustomButton : Button
+    {
+        public CustomButton()
+        {
+            SetStyle(ControlStyles.Selectable, false);
+        }
+    }
+}
